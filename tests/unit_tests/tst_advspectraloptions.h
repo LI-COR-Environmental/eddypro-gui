3e2504b2a22c45a91ef9743d1ca13340ae3ee373
Antonio Forgione <emiola@gmail.com>
2015-10-14 11:19:29 -0500

    WIP, adding unit tests.

--- tests/unit_tests/tst_advspectraloptions.h
+++ tests/unit_tests/tst_advspectraloptions.h
@@ -3,7 +3,9 @@
 
 #include <QObject>
 
-#include "testrunner.h"
+//#include "testrunner.h"
+
+#include "QtTestUtil/QtTestUtil.h"
 
 class Test_AdvSpectralOptions_Class : public QObject
 {
@@ -18,6 +20,7 @@ private slots:
     void cleanupTestCase();
 };
 
-DECLARE_TEST(Test_AdvSpectralOptions_Class)
+//DECLARE_TEST(Test_AdvSpectralOptions_Class)
+QTTESTUTIL_REGISTER_TEST(Test_AdvSpectralOptions_Class);
 
 #endif // TST_ADVSPECTRALOPTIONS_H
