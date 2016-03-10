ff2911d9796ce958ae0861110fc641c1384be598
Antonio Forgione <emiola@gmail.com>
2016-03-10 14:44:48 -0600

    Remove unneeded define.

--- src/polyfit.hpp
+++ src/polyfit.hpp
@@ -9,9 +9,6 @@
 #	undef BOOST_UBLAS_TYPE_CHECK
 #endif
 #define BOOST_UBLAS_TYPE_CHECK 0
-#ifndef _USE_MATH_DEFINES
-#	define _USE_MATH_DEFINES
-#endif
 
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/lu.hpp>
