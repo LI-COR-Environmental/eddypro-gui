36c575b76938b04adf3b77f3fdb4e9c48f1e0ff2
Antonio Forgione <emiola@gmail.com>
2016-06-06 14:32:40 -0500

    Update QuaZip prelink path to recompile with Qt-5.7.0-rc.

--- scripts/build/win-pre-link-debug.cmd
+++ scripts/build/win-pre-link-debug.cmd
@@ -1,3 +1,3 @@
 echo 'Copy dynamic libs in the debug binary folder...'
 
-copy /Y ..\libs\build-quazip-0.7.1-qt-5.6.0-mingw-4.9.2-x86_32\debug\quazipd.dll debug
+copy /Y ..\libs\build-quazip-0.7.1-qt-5.7.0-rc-mingw-5.3.0-x86_32\quazip\debug\quazipd.dll debug
