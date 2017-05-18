0c0ccb595861241ebc82c4287af898af88f963d8
Antonio Forgione <emiola@gmail.com>
2017-05-18 12:02:40 -0500

    Update build paths

--- scripts/build/mac-update-translations.sh
+++ scripts/build/mac-update-translations.sh
@@ -7,7 +7,7 @@ echo "[pwd: $PWD]"
 
 export PATH="~/Qt/5.7.0/5.7/clang_64/bin:$PATH"
 
-lupdate -verbose -no-obsolete ../../source/eddypro_mac.pro
-lrelease -verbose ../../source/eddypro_mac.pro
+lupdate -verbose -no-obsolete ../../../source/eddypro_mac.pro
+lrelease -verbose ../../../source/eddypro_mac.pro
 
 echo 'translations updated.'
