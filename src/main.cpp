5945cdf022346be5958b8d42fef3bb276530821b
Antonio Forgione <emiola@gmail.com>
2017-05-18 12:31:44 -0500

    Remove Qt::AA_UseOpenGLES flag in order to make new testing with Qt 5.8.0

--- src/main.cpp
+++ src/main.cpp
@@ -136,7 +136,7 @@ int main(int argc, char *argv[])
     /// A set of flags to workaroud issues in specific cases
     ///
     //  QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
-    QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
+    //  QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
     //  QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
     //  QCoreApplication::setAttribute(Qt::AA_ForceRasterWidgets);
     //  QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
