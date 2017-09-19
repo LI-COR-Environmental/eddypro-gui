207f16f58479561f3398841720b00a9cbb2ecf2a
Antonio Forgione <emiola@gmail.com>
2017-09-19 15:30:34 -0500

    Update to Qt 5.9.1

--- scripts/build/mac-update-translations.sh
+++ scripts/build/mac-update-translations.sh
@@ -5,7 +5,7 @@ echo 'Update translation files...'
 PWD=$(pwd)
 echo "[pwd: $PWD]"
 
-export PATH="~/Qt/5.9.0/5.9/clang_64/bin:$PATH"
+export PATH="~/Qt/5.9.1/5.9/clang_64/bin:$PATH"
 
 lupdate -verbose -no-obsolete ../../../source/eddypro_mac.pro
 lrelease -verbose ../../../source/eddypro_mac.pro
