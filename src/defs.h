048bd17aa962b7940601a8bf32244f22d02a00da
Antonio Forgione <emiola@gmail.com>
2018-10-08 17:09:21 -0500

    Bump project file version from 4.4.3 to 4.5.0

--- src/defs.h
+++ src/defs.h
@@ -55,8 +55,8 @@ namespace Defs
     const auto APP_VERSION_STR = QStringLiteral("6.2.2");
     const auto APP_STAGE_STR = QStringLiteral(""); // -rc1
 
-    const auto PROJECT_FILE_VERSION = 0x040402;
-    const auto PROJECT_FILE_VERSION_STR = QStringLiteral("4.4.3");
+    const auto PROJECT_FILE_VERSION = 0x040500;
+    const auto PROJECT_FILE_VERSION_STR = QStringLiteral("4.5.0");
 
     const auto METADATA_FILE_VERSION = 0x030201;
     const auto METADATA_FILE_VERSION_STR = QStringLiteral("3.2.2");
