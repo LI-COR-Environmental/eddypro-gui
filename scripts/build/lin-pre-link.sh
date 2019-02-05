5d731258d6d002b9761df73597c8351e4938fff3
BotoKopo <botokopo@netc.fr>
2019-02-05 07:41:31 +0100

    Update pre-link script for linux : try to find appropriate quazip building directory.

--- scripts/build/lin-pre-link.sh
+++ scripts/build/lin-pre-link.sh
@@ -2,6 +2,11 @@
 echo "Copying dynamic libraries in the debug or release build folder... "
 
 DEBUG_OR_RELEASE=$1
+if [ -n "$2" ] ; then
+    ROOT_DIR="$2"
+else
+    ROOT_DIR="../eddypro-gui"
+fi
 
 if [ "$DEBUG_OR_RELEASE" = "debug" ]; then
   QUAZIP_LIB="libquazip_debug.so.1.0.0"
@@ -15,8 +20,34 @@ echo "[pwd: $PWD]"
 BUILD_DIR="./$DEBUG_OR_RELEASE"
 echo "[BUILD_DIR: $BUILD_DIR]"
 
-QUAZIP_BUILD_DIR="build-quazip-0.7.1-qt-5.7.0-centos-gcc-4.8.5-x86_64/quazip"
+# search for any shadow building, if any
+d=`ls -d $ROOT_DIR/libs/build-${QZ}*`
+if [ -n "$d" ] ;then
+    # found at least one dir
+    if [ `echo "$d" | wc -l` -gt 1 ] ; then
+        echo "    > too many building directories : can't choose one !" ; exit 1 ;
+    fi
+    QUAZIP_BUILD_DIR="`basename $d`"
+else
+# otherwise, use default in-place building
+    QUAZIP_BUILD_DIR="quazip-0.7.1/quazip"
+fi
+
 echo "[QUAZIP_BUILD_DIR: $QUAZIP_BUILD_DIR]"
 
 echo "Copy quazip in the app binary folder..."
-cp "../libs/$QUAZIP_BUILD_DIR/$QUAZIP_LIB" "$BUILD_DIR"
+cp "$ROOT_DIR/libs/$QUAZIP_BUILD_DIR/$QUAZIP_LIB" "$BUILD_DIR"
+# link to .so file (for ld building)
+qzlib=`basename $QUAZIP_LIB .1.0.0`
+ln -s "$ROOT_DIR/libs/$QUAZIP_BUILD_DIR/$QUAZIP_LIB" "$BUILD_DIR/$qzlib"
+# link to .so.1 file (for binary linking)
+qzlib=`basename $QUAZIP_LIB .1.0.0`
+ln -s "$ROOT_DIR/libs/$QUAZIP_BUILD_DIR/$QUAZIP_LIB" "$BUILD_DIR/$qzlib.1"
+# add a launching wrapper, as a quick hack for installation lack
+cat > "$BUILD_DIR/eddypro.sh" << EOF
+#!/bin/sh
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:\`dirname \$0\` \$0"
+./eddypro_${DEBUG_OR_RELEASE}
+EOF
+chmod +x "$BUILD_DIR/eddypro.sh"
+
