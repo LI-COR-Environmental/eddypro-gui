14dcc72c92c9ed2cdeef67a77d86f18fe3102952
BotoKopo <botokopo@netc.fr>
2019-02-06 07:33:49 +0100

    Add one-shot building script for linux (one might not use qtcreator).

---
+++ scripts/build/lin-build-eddypro.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# shadow build in debug or release
+# usage
+# $ scripts/build/lin-build-eddypro.sh [debug|release]
+
+echo "### Running '$ $0 $@' in '$PWD'..."
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 [debug|release]\n\tMust be launched from project's root directory" 1>&2
+    exit 1
+fi
+if [ "$1" = "debug" ] ; then
+    DEBUG_OR_RELEASE=$1
+else
+    DEBUG_OR_RELEASE="release"
+fi
+if [ ! -f eddypro_lin.pro ] ; then
+  echo "seems not to be launched from project's root directory" >&2
+  exit 1
+fi
+SRC_DIR="$PWD"
+
+# build libs
+$SRC_DIR/scripts/build/lin-build-quazip.sh $DEBUG_OR_RELEASE
+
+# update ressource
+$SRC_DIR/scripts/build/lin-update-ressources.sh $DEBUG_OR_RELEASE
+
+# set build directory
+app_name="eddypro"
+qt_version="qt-`qmake -v | grep Qt | cut -d ' ' -f 4`"
+distrib=`{ lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | grep PRETTY_NAME  cut -d = -f 2 ; } | sed 's/ /_/g;s/"//g;'`
+[ -z "$distrib" ] && distrib="linux"
+platform=`uname -m`
+build_dir="build-$app_name-$DEBUG_OR_RELEASE+$qt_version+$distrib+$platform"
+
+# set qmake project file
+qmake_project="$SRC_DIR/eddypro_lin.pro"
+
+# make shadow build
+#SHADOW_DIR="../build/$build_dir"
+SHADOW_DIR="../$build_dir"
+mkdir -p "$SHADOW_DIR"
+
+cd "$SHADOW_DIR"
+echo "### Make shadow build in '$PWD'..."
+
+echo "### Run 'qmake'..."
+qmake -Wall $qmake_project
+
+echo "### Run 'make'..."
+make $DEBUG_OR_RELEASE
+
+
