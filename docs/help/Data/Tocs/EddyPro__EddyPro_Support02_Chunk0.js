d517d3d893a800c8708adce211b95caab89a9e56
Antonio Forgione <emiola@gmail.com>
2019-02-27 16:24:28 -0600

    Update help files.

--- docs/help/Data/Tocs/EddyPro__EddyPro_Support02_Chunk0.js
+++ docs/help/Data/Tocs/EddyPro__EddyPro_Support02_Chunk0.js
@@ -1,1 +1,1 @@
-define({'/EddyPro/docs.html':{i:[2],t:['All Documents'],b:['']},'/EddyPro/faqs.html':{i:[1],t:['FAQs'],b:['']},'/EddyPro/manuals.html':{i:[0],t:['Instruction Manuals'],b:['']},'http://www.licor.com/env/forum/?forum=eddypro':{i:[3],t:['Forum'],b:['']}});
+define({'/EddyPro/docs.html':{i:[3],t:['All Documents'],b:['']},'/EddyPro/faqs.html':{i:[2],t:['FAQs'],b:['']},'/EddyPro/manuals.html':{i:[0],t:['Instruction Manuals'],b:['']},'/EddyPro/videos.html':{i:[1],t:['Videos'],b:['']}});
